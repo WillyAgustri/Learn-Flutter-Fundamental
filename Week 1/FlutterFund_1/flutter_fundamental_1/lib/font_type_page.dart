021df2cf048795cb4a5b3435d9d151554b580ce3
WillyAgustri <willyofficial082@gmail.com>
2024-01-05 01:24:29 +0700

    add Font Style Page

---
+++ Week 1/FlutterFund_1/flutter_fundamental_1/lib/font_type_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FontTypePage extends StatefulWidget {
+  const FontTypePage({super.key});
+
+  @override
+  State<FontTypePage> createState() => _FontTypePageState();
+}
+
+class _FontTypePageState extends State<FontTypePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Font Type")),
+      body: Center(
+        child: SizedBox(
+          child: Text("Ini Adalah Font Type"),
+        ),
+      ),
+    );
+  }
+}
