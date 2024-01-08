6eeead5b02944694594c335d73280eea505ba37f
WillyAgustri <willyofficial082@gmail.com>
2024-01-09 02:07:32 +0700

    Add Desc in Container 1

--- Week 2/flutter_fundamental_2/lib/gridView_page.dart
+++ Week 2/flutter_fundamental_2/lib/gridView_page.dart
@@ -15,21 +15,25 @@ class GridViewPage extends StatelessWidget {
             width: 100,
             height: 100,
             color: Color.fromARGB(255, 225, 99, 179),
+            child: Text("Data 2"),
           ),
           Container(
             width: 100,
             height: 100,
             color: Color.fromARGB(255, 75, 79, 118),
+            child: Text("Data 2"),
           ),
           Container(
             width: 100,
             height: 100,
             color: Colors.amber,
+            child: Text("Data 2"),
           ),
           Container(
             width: 100,
             height: 100,
             color: Colors.blueAccent,
+            child: Text("Data 2"),
           )
         ],
       ),
