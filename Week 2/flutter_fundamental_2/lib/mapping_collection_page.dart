52ff2bb1660602fc89a6d39365135744f2fc8c91
WillyAgustri <willyofficial082@gmail.com>
2024-01-15 02:01:52 +0700

    Use List.Generate

--- Week 2/flutter_fundamental_2/lib/mapping_collection_page.dart
+++ Week 2/flutter_fundamental_2/lib/mapping_collection_page.dart
@@ -6,29 +6,15 @@ class MappingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<colorBox> allItems = List.generate(
+        10, (index) => colorBox(keterangan: "Kotak =", warna: Colors.black12));
+
     return Scaffold(
         appBar: AppBar(
           title: Text("Learn Mapping"),
         ),
-        body: ListView(
-          children: [
-            colorBox(keterangan: "Kotak 1", warna: Colors.black12),
-            colorBox(
-                keterangan: "Kotak 2", warna: Color.fromARGB(31, 238, 9, 9)),
-            colorBox(
-                keterangan: "Kotak 3",
-                warna: const Color.fromARGB(31, 143, 41, 41)),
-            colorBox(
-                keterangan: "Kotak 4", warna: Color.fromARGB(31, 40, 65, 208)),
-            colorBox(
-                keterangan: "Kotak 5", warna: Color.fromARGB(31, 94, 139, 53)),
-            colorBox(
-                keterangan: "Kotak 6", warna: Color.fromARGB(31, 94, 139, 53)),
-            colorBox(
-                keterangan: "Kotak 7", warna: Color.fromARGB(31, 94, 139, 53)),
-            colorBox(
-                keterangan: "Kotak 8", warna: Color.fromARGB(31, 94, 139, 53)),
-          ],
+        body: Column(
+          children: allItems,
         ));
   }
 }
