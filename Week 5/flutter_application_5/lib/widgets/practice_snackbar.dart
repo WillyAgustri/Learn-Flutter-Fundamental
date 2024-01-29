86967fca2aaa832f7f9f656ce7c70fbc0192e456
WillyAgustri <willyofficial082@gmail.com>
2024-01-30 00:43:48 +0700

    add practice snackbar

---
+++ Week 5/flutter_application_5/lib/widgets/practice_snackbar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class practiceSnackbar extends StatefulWidget {
+  const practiceSnackbar({super.key});
+
+  @override
+  State<practiceSnackbar> createState() => _practiceSnackbarState();
+}
+
+class _practiceSnackbarState extends State<practiceSnackbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text("SnackBar"),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text("Stop Click That!")));
+              },
+              child: Text("Show SnackBar"))),
+    );
+  }
+}
