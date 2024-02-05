7f2947fcec98579fa5cc607976cf41990a2c9987
WillyAgustri <willyofficial082@gmail.com>
2024-02-05 14:56:02 +0700

    fix practice tabBar & tabBar manual

--- Week 5/flutter_application_5/lib/widgets/practice_tabBar_manual.dart
+++ Week 5/flutter_application_5/lib/widgets/practice_tabBar_manual.dart
@@ -7,59 +7,62 @@ class practice_tabBar_manual extends StatefulWidget {
   State<practice_tabBar_manual> createState() => _practice_tabBar_manualState();
 }
 
-class _practice_tabBar_manualState extends State<practice_tabBar_manual> {
+class _practice_tabBar_manualState extends State<practice_tabBar_manual>
+    with TickerProviderStateMixin {
+  late TabController tabC = TabController(length: 4, vsync: this);
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-          appBar: AppBar(
-            leading: Icon(
-              Icons.wechat,
-              color: Colors.white,
-              size: 60,
-            ),
-            centerTitle: false,
-            backgroundColor: Colors.teal,
-            title: Text(
-              "WhatsApp",
-              style: TextStyle(fontSize: 30, color: Colors.white),
-            ),
-            bottom: TabBar(automaticIndicatorColorAdjustment: false, tabs: [
-              Tab(
-                child: Icon(
-                  Icons.camera_alt,
-                  color: Colors.white,
-                ),
-              ),
-              Tab(
-                child: Text(
-                  "Chat",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-              ),
-              Tab(
-                child: Text(
-                  "Status",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-              ),
-              Tab(
-                child: Text(
-                  "History Call",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-              ),
-            ]),
+    return Scaffold(
+        appBar: AppBar(
+          leading: Icon(
+            Icons.wechat,
+            color: Colors.white,
+            size: 60,
           ),
-          body: TabBarView(
-            children: [
-              Text("Camera"),
-              Text("Chat"),
-              Text("Status"),
-              Text("History Call")
-            ],
-          )),
-    );
+          centerTitle: false,
+          backgroundColor: Colors.teal,
+          title: Text(
+            "WhatsApp",
+            style: TextStyle(fontSize: 30, color: Colors.white),
+          ),
+          bottom: TabBar(
+              controller: tabC,
+              automaticIndicatorColorAdjustment: false,
+              tabs: [
+                Tab(
+                  child: Icon(
+                    Icons.camera_alt,
+                    color: Colors.white,
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    "Chat",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    "Status",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    "History Call",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+              ]),
+        ),
+        body: TabBarView(
+          controller: tabC,
+          children: [
+            Text("Camera"),
+            Text("Chat"),
+            Text("Status"),
+            Text("History Call")
+          ],
+        ));
   }
 }
