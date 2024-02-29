d066263e27ab784e41ab2b82c2eec73f494d49d8
WillyAgustri <willyofficial082@gmail.com>
2024-03-01 02:29:18 +0700

    style: add get data api

--- Week 9/flutter_week9/lib/dataMahasiswa/widget/home.dart
+++ Week 9/flutter_week9/lib/dataMahasiswa/widget/home.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
 
 class home extends StatefulWidget {
   const home({super.key});
@@ -8,6 +12,11 @@ class home extends StatefulWidget {
 }
 
 class _homeState extends State<home> {
+  List<dynamic> getDataMahasiswa = [{}];
+
+  late TextEditingController getName = TextEditingController();
+  var response;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +43,7 @@ class _homeState extends State<home> {
                         width: 100,
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(9),
-                            color: Colors.green),
+                            color: Color(0xF6495ed5)),
                       ),
                       Text("PDDKTI")
                     ],
@@ -60,6 +69,7 @@ class _homeState extends State<home> {
                   Container(
                     margin: EdgeInsets.all(15),
                     child: TextField(
+                      controller: getName,
                       decoration: InputDecoration(
                           filled: true, // Mengaktifkan pengisian latar belakang
                           fillColor: Color(0xFFE8E8E8), // Warna latar belakang
@@ -74,7 +84,22 @@ class _homeState extends State<home> {
                           shape: MaterialStatePropertyAll(
                               RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(8)))),
-                      onPressed: () {},
+                      onPressed: () async {
+                        response = await http.get(Uri.parse(
+                            "https://api-frontend.kemdikbud.go.id/hit_mhs/${getName.text}"));
+                        var data = (jsonDecode(response.body)
+                            as Map<String, dynamic>)["mahasiswa"];
+
+                        print(response.statusCode);
+                        setState(() {
+                          if (getDataMahasiswa.length > 1) {
+                            getDataMahasiswa.clear();
+                          }
+                          data.forEach((value) {
+                            getDataMahasiswa.add(value);
+                          });
+                        });
+                      },
                       child: Text(
                         "Cari",
                         style: TextStyle(
@@ -85,16 +110,35 @@ class _homeState extends State<home> {
             ),
             Expanded(
               child: ListView.builder(
-                itemCount: 5,
-                itemBuilder: (context, index) => Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: Color(0xF6495ed5)),
-                  margin: EdgeInsets.all(10),
-                  width: double.maxFinite,
-                  height: 100,
-                ),
-              ),
+                  itemCount: getDataMahasiswa.length,
+                  itemBuilder: (context, index) {
+                    print(getDataMahasiswa.length);
+                    if (getDataMahasiswa.length == 1) {
+                      return Text("Data Kosong");
+                    } else {
+                      return Container(
+                          padding: EdgeInsets.all(10),
+                          margin: EdgeInsets.all(10),
+                          height: 150,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 10.0,
+                                spreadRadius: 1.0,
+                                offset: Offset(1.0, 4.0),
+                              ),
+                            ],
+                          ),
+                          child: Text(
+                            "${getDataMahasiswa[index]["text"]}",
+                            style: TextStyle(fontSize: 18),
+                          ));
+                    }
+                  }),
             ),
           ],
         ),
