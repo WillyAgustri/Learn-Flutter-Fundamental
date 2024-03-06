212e16ffa5d884f7ba3954cf9e4c622480af70d0
WillyAgustri <willyofficial082@gmail.com>
2024-03-07 01:26:00 +0700

    feat : add practice Avatar glow

---
+++ Week 9/flutter_week9/lib/practiceAvatarGlow/practice_avatar_glow.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class practice_avatar_glow extends StatelessWidget {
+  const practice_avatar_glow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Practice Avatar Glow"),
+      ),
+    );
+  }
+}
