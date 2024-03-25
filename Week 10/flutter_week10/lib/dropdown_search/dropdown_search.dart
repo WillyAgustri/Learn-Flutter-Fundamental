46747ebd7dd13f7aeb9c6c2afb14624eb0f6d562
WillyAgustri <willyofficial082@gmail.com>
2024-03-26 00:28:11 +0700

    add week 11

--- Week 10/flutter_week10/lib/dropdown_search/dropdown_search.dart
+++ Week 10/flutter_week10/lib/dropdown_search/dropdown_search.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:dropdown_search/dropdown_search.dart';
+import 'UserModel/user.dart';
+
 class dropdown_search extends StatefulWidget {
   const dropdown_search({super.key});
 
@@ -8,6 +11,48 @@ class dropdown_search extends StatefulWidget {
 }
 
 class _dropdown_searchState extends State<dropdown_search> {
+  var id;
+  var createdAt;
+  var nama;
+
+  List<UserModel> users = [
+    UserModel(
+      id: "1",
+      createdAt: DateTime.now(),
+      name: "John Doe",
+      avatar: "https://example.com/avatar1.jpg",
+    ),
+    UserModel(
+      id: "2",
+      createdAt: DateTime.now(),
+      name: "Jane Smith",
+      avatar: "https://example.com/avatar2.jpg",
+    ),
+    UserModel(
+      id: "3",
+      createdAt: DateTime.now(),
+      name: "Alice Johnson",
+      avatar: "https://example.com/avatar3.jpg",
+    ),
+    UserModel(
+      id: "4",
+      createdAt: DateTime.now(),
+      name: "Bob Johnson",
+      avatar: "https://example.com/avatar4.jpg",
+    ),
+    UserModel(
+      id: "5",
+      createdAt: DateTime.now(),
+      name: "Emily Brown",
+      avatar: "https://example.com/avatar5.jpg",
+    ),
+    UserModel(
+        id: "6",
+        createdAt: DateTime.now(),
+        name: "Gilowo",
+        avatar: "https://google.com")
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +61,56 @@ class _dropdown_searchState extends State<dropdown_search> {
           title: Text(
             "DropDown Search",
           )),
-      body: Center(),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Center(
+          child: Container(
+              child: Column(
+            children: [
+              DropdownSearch<UserModel>(
+                popupProps: PopupProps.menu(showSearchBox: true),
+                items: users,
+                onChanged: (UserModel? data) {
+                  setState(() {
+                    id = data?.id;
+                    nama = data?.name;
+                    createdAt = data?.createdAt;
+                  });
+                },
+                dropdownDecoratorProps: DropDownDecoratorProps(
+                  dropdownSearchDecoration:
+                      InputDecoration(labelText: "Tipe Pengumuman"),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Detail Informasi :",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                  width: double.maxFinite,
+                  height: 400,
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("ID : " + id.toString()),
+                      Text("Nama : " + nama.toString()),
+                      Text("Dibuat : " + createdAt.toString())
+                    ],
+                  )),
+            ],
+          )),
+        ),
+      ),
     );
   }
 }
